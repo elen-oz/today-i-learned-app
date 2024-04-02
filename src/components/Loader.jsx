deae934856e56dd1cc5964086726f9d9a6571731
elen-oz <ozerova.lenka@gmail.com>
2024-04-02 16:10:50 +0200

    refactor: change loading message

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,17 @@
 const Loader = () => {
   return (
     <>
-      <p className='message'>Loading...</p>
       <p className='message'>
+        Loading...
+        <br />
+        <br />
         PS: As I have free access to supabase - due to inactivity for 7 days,
         they may suspend access to the data base and it will need to be manually
         resumed. If the fact cards are not displayed on the site, it means that
-        the project is on pause. It is in the process of moving to another
-        platform.
+        the project is on pause.
+        <br />
+        <br />
+        It is in the process of moving to another platform.
       </p>
     </>
   );
